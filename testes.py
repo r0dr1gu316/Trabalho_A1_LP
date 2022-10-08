e4b0484cdcd90508a8df00d3774fc82faf6a3020
Caue_Rodrigues <105641544+r0dr1gu316@users.noreply.github.com>
2022-10-08 16:08:26 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -38,7 +38,6 @@ def pegar_info_tempo(link):
 
 
 ##############################
-
 musicas_transpiracao = pegar_info_nome('https://pt.wikipedia.org/wiki/Transpira%C3%A7%C3%A3o_Cont%C3%ADnua_Prolongada')
 del musicas_transpiracao[16:]
 #print(musicas_transpiracao)
@@ -48,7 +47,6 @@ del tempos_transpiracao[16:]
 #print(tempos_transpiracao)
 
 ##############################
-
 musicas_preco = pegar_info_nome('https://pt.wikipedia.org/wiki/Pre%C3%A7o_Curto..._Prazo_Longo')
 #print(musicas_preco)
 
@@ -56,7 +54,6 @@ tempos_preco = pegar_info_tempo('https://pt.wikipedia.org/wiki/Pre%C3%A7o_Curto.
 #print(tempos_preco)
 
 ##############################
-
 musicas_nadando = pegar_info_nome('https://pt.wikipedia.org/wiki/Nadando_com_os_Tubar%C3%B5es')
 #print(musicas_nadando)
 
@@ -64,7 +61,6 @@ tempos_nadando = pegar_info_tempo('https://pt.wikipedia.org/wiki/Nadando_com_os_
 #print(tempos_nadando)
 
 ##############################
-
 musicas_abalando = pegar_info_nome('https://pt.wikipedia.org/wiki/100%25_Charlie_Brown_Jr._-_Abalando_a_Sua_F%C3%A1brica')
 #print(musicas_abalando)
 
@@ -72,7 +68,6 @@ tempos_abalando = pegar_info_tempo('https://pt.wikipedia.org/wiki/100%25_Charlie
 #print(tempos_abalando)
 
 ##############################
-
 musicas_bocas = pegar_info_nome('https://pt.wikipedia.org/wiki/Bocas_Ordin%C3%A1rias')
 #print(musicas_bocas)
 
@@ -80,7 +75,6 @@ tempos_bocas = pegar_info_tempo('https://pt.wikipedia.org/wiki/Bocas_Ordin%C3%A1
 #print(tempos_bocas)
 
 ##############################
-
 musicas_tamoai = pegar_info_nome('https://pt.wikipedia.org/wiki/Tamo_A%C3%AD_na_Atividade')
 #print(musicas_tamoai)
 
@@ -88,7 +82,6 @@ tempos_tamoai = pegar_info_tempo('https://pt.wikipedia.org/wiki/Tamo_A%C3%AD_na_
 #print(tempos_tamoai)
 
 ##############################
-
 musicas_imunidade = pegar_info_nome('https://pt.wikipedia.org/wiki/Imunidade_Musical')
 #print(musicas_imunidade)
 
@@ -96,7 +89,6 @@ tempos_imunidade = pegar_info_tempo('https://pt.wikipedia.org/wiki/Imunidade_Mus
 #print(tempos_imunidade)
 
 ##############################
-
 musicas_ritmo = pegar_info_nome('https://pt.wikipedia.org/wiki/Ritmo,_Ritual_e_Responsa')
 #print(musicas_ritmo)
 
@@ -104,7 +96,6 @@ tempos_ritmo = pegar_info_tempo('https://pt.wikipedia.org/wiki/Ritmo,_Ritual_e_R
 #print(tempos_ritmo)
 
 ##############################
-
 musicas_camisa = pegar_info_nome('https://pt.wikipedia.org/wiki/Camisa_10_Joga_Bola_At%C3%A9_na_Chuva')
 #print(musicas_camisa)
 
@@ -117,14 +108,13 @@ for string in tempos_camisa:
 #print(tempos_camisa_novo)
 
 ################################
-
 musicas_013 = pegar_info_nome('https://pt.wikipedia.org/wiki/La_Familia_013')
 #print(musicas_013)
 
 tempos_013 = pegar_info_tempo('https://pt.wikipedia.org/wiki/La_Familia_013')
 #print(tempos_013)
-################################
 
+################################
 musicas_charlie = pegar_info_nome('https://pt.wikipedia.org/wiki/Ac%C3%BAstico_MTV:_Charlie_Brown_Jr.')
 #print(musicas_charlie)
 
@@ -142,16 +132,27 @@ tempos_basica = pegar_info_tempo('https://pt.wikipedia.org/wiki/M%C3%BAsica_Popu
 
 
 df_duracao_transpiracao = pd.DataFrame(zip(musicas_transpiracao, tempos_transpiracao), columns = ['Nome da Música', 'Duração da Música'])
+
 df_duracao_preco = pd.DataFrame(zip(musicas_preco, tempos_preco), columns = ['Nome da Música', 'Duração da Música'])
+
 df_duracao_nadando = pd.DataFrame(zip(musicas_nadando, tempos_nadando), columns = ['Nome da Música', 'Duração da Música'])
+
 df_duracao_abalando = pd.DataFrame(zip(musicas_abalando, tempos_abalando), columns = ['Nome da Música', 'Duração da Música'])
+
 df_duracao_bocas= pd.DataFrame(zip(musicas_bocas, tempos_bocas), columns = ['Nome da Música', 'Duração da Música'])
+
 df_duracao_tamoai = pd.DataFrame(zip(musicas_tamoai, tempos_tamoai), columns = ['Nome da Música', 'Duração da Música'])
+
 df_duracao_imunidade = pd.DataFrame(zip(musicas_imunidade, tempos_imunidade), columns = ['Nome da Música', 'Duração da Música'])
+
 df_duracao_ritmo = pd.DataFrame(zip(musicas_ritmo, tempos_ritmo), columns = ['Nome da Música', 'Duração da Música'])
+
 df_duracao_camisa = pd.DataFrame(zip(musicas_camisa, tempos_camisa_novo), columns = ['Nome da Música', 'Duração da Música'])
+
 df_duracao_013 = pd.DataFrame(zip(musicas_013, tempos_013), columns = ['Nome da Música', 'Duração da Música'])
+
 df_duracao_charlie = pd.DataFrame(zip(musicas_charlie, tempos_charlie), columns = ['Nome da Música', 'Duração da Música'])
+
 df_duracao_basica = pd.DataFrame(zip(musicas_basica, tempos_basica), columns = ['Nome da Música', 'Duração da Música'])
 
 
