c8040b0dc6e62b9c0e2664a79f780857046449b3
Caue_Rodrigues <105641544+r0dr1gu316@users.noreply.github.com>
2022-10-08 18:22:38 -0300

    Update grupo 3.py

--- grupo 3.py
+++ grupo 3.py
@@ -1,1 +1,7 @@
+import pandas as pd
+from coleta_manual import ordem_albunsdf
+from coleta_manual import data_lanc_albuns
+from IPython.display import display 
 
+df_albuns = pd.DataFrame({'Lançamento':data_lanc_albuns}, index=[ordem_albunsdf])
+#display(df_albuns)
